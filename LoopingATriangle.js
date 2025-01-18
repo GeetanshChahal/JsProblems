4fd50e2d3b77ff439eac9648de91fa6e8496dd8d
Geetansh Chahal <78197749+GeetanshChahal@users.noreply.github.com>
2025-01-18 15:08:37 +0000

    Looping a triangle solution

---
+++ LoopingATriangle.js
@@ -0,0 +1,9 @@
+for(let i=0;i<7;i++){
+    let row="";
+    for(let j=0;j<=i;j++){
+        row+="#";
+    }
+    console.log(row);
+}
+
+// Solution of Looping a triangle problem from Eloquent Javascript Book
