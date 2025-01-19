5f4c77f567b717e0bfb24f6d69eb055836342c95
Geetansh Chahal <78197749+GeetanshChahal@users.noreply.github.com>
2025-01-19 18:06:41 +0000

    MathsMinimum solution

---
+++ MathsMinimum.js
@@ -0,0 +1,11 @@
+function minimum(a, b) {
+    return a < b ? a : b;
+}
+
+
+console.log(minimum(10,15));
+console.log(minimum(45,20));
+console.log(minimum(-8,18));
+
+// Solution of FizzBuzz problem from Eloquent Javascript Book
+// Write a function minimum(a, b) that returns the smaller of two numeric values
